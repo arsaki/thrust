765ad90601105deda49b95eedee2739070c19b3f
Arseniy Akimov <a.akimov@aerodisk.ru>
2022-11-18 10:01:39 +0300

    formated and some variables renamed, change calibrations const , fixed time accuracy

--- project_thrust/project_thrust.ino
+++ project_thrust/project_thrust.ino
@@ -1,7 +1,8 @@
-/*  Anna S. a.k.a plokmit
+/*  
+ *  Author Anna S. a.k.a plokmit
  *  2022
  *  Distributed free
- *  Rocket test rig main program
+ *  Rocket engine test rig program
  */
 
 #include <ESP8266WebServer.h>
@@ -9,36 +10,38 @@
 
 #define DT  		12
 #define SCK 		13
-#define ARRAY_LENGTH	1024
+/* Thrust measurement data array length */
+#define ARRAY_LENGTH	  1024
+#define LOGO_JPEG_SIZE  81312
 
-extern const char logo_jpeg[81312];
-extern const char webpage[] PROGMEM;
+/* logo_thrust.ino */
+extern const char logo_jpeg[LOGO_JPEG_SIZE];
+/* web.htm */
+extern const char webpage[];
 
-const char *ssid = "ESPAP"; 
+const char *ssid     = "Project Thrust"; 
 const char *password = "";
 
-const double calibrationCoefficient = 17.31; 
-const double unitsToKg = 0.035274
-/* Moscow region acceleration */
-const double gMoscow = 9.8154;    
+const double calibrationCoefficient = 17.233826;
+const double unitsToKg = 0.035274;
+/* Moscow region gravitational constant */
+const double gMoscow   = 9.8154;    
 
-
-int count = 0;
-double arr[ARRAY_LENGTH]; 
-double newtons; 
-double sec = 0;
+int    count   = 0;
+double sec     = 0;
 double impulse = 0;
+double newtons; 
+/* Thrust measurement data array */
+double thrust_array[ARRAY_LENGTH]; 
 String measureState ="0"; 
 
 HX711 scale;
 ESP8266WebServer server(80); 
 
 
-
 void mainHTMLPage()
 {
-  String s = webpage; /*web.htm*/
-  server.send(200, "text/html",s); 
+  server.send(200, "text/html",webpage); 
 }
 
 void sendImage()
@@ -55,26 +58,26 @@ void setStateMeasure()
 void sendArray() 
 {
   String arrStr;
-  for (int i = 0; i < ARRAY_LENGTH+COUNT; i++)
-    arrStr += (String)arr[i] + " ";
-  server.send(200,"text/html", arrStr);
+  for (int i = 0; i < ARRAY_LENGTH; i++)
+    arrStr += (String)thrust_array[i] + " ";
+  server.send(200, "text/html", arrStr);
 }
 
 void sendThrust()
 {
-  String thrust =(String) (scale.get_units()*unitsToKg*gMoscow/1000);
-  server.send(200,"text/plane", thrust);
+  String thrust =(String)(scale.get_units()*unitsToKg*gMoscow/1000);
+  server.send(200, "text/plane", thrust);
 }
 
 void sendImpulse()
 {
-  impulse=0;
-  for (int i=0; i<ARRAY_LENGTH; i++){
-    if((String)arr[i] != "nan"){
-      impulse = impulse + (arr[i]*0.1);
+  impulse = 0;
+  for (int i = 0; i < ARRAY_LENGTH; i++){
+    if((String)thrust_array[i] != "nan"){
+      impulse = impulse + (thrust_array[i]*0.1);
     }
   }
-  server.send(200,"text/plane", (String)impulse);
+  server.send(200, "text/plane", (String)impulse);
 }
 
 void sendFile() 
@@ -82,7 +85,7 @@ void sendFile()
   String file;
   for (int i = 0; i < count; i++){
     sec += 0.1;
-    file += (String)arr[i] + "," + sec +"," + "\n";
+    file += (String)thrust_array[i] + "," + sec +"," + "\n";
   }
   server.send(200,"text/csv", file);
 }
@@ -95,7 +98,7 @@ void calibrate()
 void clearArray() 
 {
   for (int i = 0; i < ARRAY_LENGTH; i++)
-    arr[i] = NAN;
+    thrust_array[i] = NAN;
   count = 0;
   sec = 0;
 }
@@ -108,17 +111,16 @@ void setup(void)
   Serial.print("IP address: ");
   Serial.println(WiFi.softAPIP());
   scale.begin(DT, SCK); 
-  scale.set_scale();  
-  scale.tare();
   scale.set_scale(calibrationCoefficient);
-  server.on("/",mainHTMLPage);
+  scale.tare();
+  server.on("/",     mainHTMLPage);
   server.on("/logo", sendImage);
-  server.on("/state_measure", setStateMeasure);
-  server.on("/array", sendArray);
-  server.on("/thrust", sendThrust);
-  server.on("/impulse", sendImpulse);
-  server.on("/file.csv",sendFile);
+  server.on("/file.csv",  sendFile);
   server.on("/calibrate", calibrate);
+  server.on("/state_measure", setStateMeasure);
+  server.on("/thrust",  sendThrust);
+  server.on("/impulse", sendImpulse);
+  server.on("/array",   sendArray);
   server.begin(); 
   clearArray();
 }
@@ -128,12 +130,12 @@ void loop()
   server.handleClient(); 
   if (measureState == "1")
     clearArray();
-  for (int i = 0; (measureState=="1") && (i<ARRAY_LENGTH); i++) {
+  for (int i = 0; (measureState == "1") && (i < ARRAY_LENGTH); i++) {
     if(i == ARRAY_LENGTH - 1)
       measureState = "0";
     delay(87);
     newtons = scale.get_units()*unitsToKg*gMoscow/1000;
-    arr[i] = newtons;
+    thrust_array[i] = newtons;
     count++;
     server.handleClient();
   }
